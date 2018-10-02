ba1691fa739901743fe4596a670e5ac2ed2631d2
Oliver Boudet <oboudet1@me.com>
2018-10-02 14:52:54 +0200

    add PointSpend

--- GetDivide/GetDivide/Program.cs
+++ GetDivide/GetDivide/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 namespace GetDivide {
     internal class Program {
         public static void Main(string[] args) {
+            Spinner spin = new Spinner(0,0);
             int input;
             try {
                 input = int.Parse(Console.ReadLine());
@@ -14,10 +15,12 @@ namespace GetDivide {
                 Main(args);
                 return;
             }
+            spin.Start();
             List<int> nums = GetDivide(input);
             foreach (int num in nums) {
                 Console.WriteLine(num);
             }
+            spin.Stop();
         }
 
         static List<int> GetDivide(int max) {
---
+++ PointSpend/PointSpend/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PointSpend {
+    internal class Program {
+        public static void Main(string[] args) {
+            // Declarations
+
+            int input = 0;
+            int pointToSpend = 12;
+            bool keepGoing = true;
+            
+            // This is incredibly ugly.
+            Console.WriteLine("Welcome! Please create your character.\nYou have 12to spend on Strength.\nHow many points would you like to spend on Strength? At least 1!");
+            while (keepGoing) {
+                try {
+                    input = int.Parse(Console.ReadLine());
+                    keepGoing = false;
+                }
+                catch (Exception e) {
+                    keepGoing = true;
+                    Console.WriteLine(e);
+                    throw;
+                }
+
+                if (!validate(input)) {
+                    Console.WriteLine("Please enter a number between 1 and 12.");
+                    keepGoing = true;
+                }
+            }
+
+            pointToSpend -= input;
+            Console.WriteLine($"All done!\n---------\nPoints left: {pointToSpend}");
+        }
+
+        static bool validate(int num) {
+            return num > 0 && num <= 12;
+        }
+    }
+}
