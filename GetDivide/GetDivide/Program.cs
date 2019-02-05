ef5672f44e1262af05d28a74542d26d363c4675b
Oliver Boudet <oboudet1@me.com>
2019-02-05 14:18:15 +0100

    Update GetDivide

--- GetDivide/GetDivide/Program.cs
+++ GetDivide/GetDivide/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GetDivide {
     internal class Program {
         public static void Main(string[] args) {
             //Spinner spin = new Spinner(0,0);
-            int input;
+            int input = 0;
             try {
                 input = int.Parse(Console.ReadLine());
             }
@@ -13,17 +14,16 @@ namespace GetDivide {
                 Console.WriteLine("Please input a number.");
                 // R E C U R S I O N
                 Main(args);
-                return;
             }
-            //spin.Start();
-            List<int> nums = GetDivide(input);
+            
+            // Sure I could do a reverse forloop but foreach is 👌.
+            IEnumerable<int> nums = GetDivide(input).Reverse();
             foreach (int num in nums) {
                 Console.WriteLine(num);
-            }
-            //spin.Stop();
+            }   
         }
 
-        static List<int> GetDivide(int max) {
+        static IEnumerable<int> GetDivide(int max) {
             List<int> nums = new List<int>();
             for (int i = 0; i < max; i++) {
                 if (i % 3 == 0) {
