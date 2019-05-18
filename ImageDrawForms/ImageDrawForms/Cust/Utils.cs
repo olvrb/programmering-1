da0e4aa806561a002c5e671a1306afbb08e55652
Oliver Boudet <oboudet1@me.com>
2019-05-18 10:33:36 +0200

    forgot to push

--- ImageDrawForms/ImageDrawForms/Cust/Utils.cs
+++ ImageDrawForms/ImageDrawForms/Cust/Utils.cs
@@ -13,15 +13,12 @@ namespace ImageDrawForms {
 
         public static string GetFileFromPrompt() {
             FileDialog dialog = new OpenFileDialog();
-            if (dialog.ShowDialog() == DialogResult.OK) {
-                return dialog.FileName;
-            }
-
-            return "";
+            // Return null if the user cancelled the prompt.
+            return dialog.ShowDialog() == DialogResult.OK ? dialog.FileName : null;
         }
 
         // Basically just a wrapper for Task.Run...
-        // Wannabe objective c dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{  })
+        // Wannabe objective c dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{  });
         public static Task DispatchAsync(Action func) {
             return Task.Run(func);
         }
